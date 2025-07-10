f6253bcf04e267e4ee7f1090481284f67e6a9b25
albermoon <albertoluna92@gmail.com>
2025-07-10 13:47:15 +0200

    Simplification of the call to the backend

--- lib/api_routes/messages_api.dart
+++ lib/api_routes/messages_api.dart
@@ -1,10 +1,10 @@
 import 'package:api/client/cococare_client.dart';
 
 class MessagesApi {
-  static Future<(Map<String, dynamic>?, String)> postMessageToAll(Map<String, dynamic> data) async {
+  static Future<(Map<String, dynamic>?, String)> postMessage(Map<String, dynamic> data) async {
     try {
       final client = CococareApiClient.instance;
-      final endpoint = Uri.parse('${client.baseUrl}/messages/all');
+      final endpoint = Uri.parse('${client.baseUrl}/messages/');
       final response = await client.dio.postUri(endpoint, data: data);
       return (response.data as Map<String, dynamic>?, '');
     } catch (e) {
@@ -12,34 +12,26 @@ class MessagesApi {
     }
   }
 
-  static Future<(Map<String, dynamic>?, String)> postMessageToPatient(String patientId, Map<String, dynamic> data) async {
+  static Future<(List<dynamic>?, String)> getSentMessages() async {
     try {
       final client = CococareApiClient.instance;
-      final endpoint = Uri.parse('${client.baseUrl}/messages/patient/$patientId');
-      final response = await client.dio.postUri(endpoint, data: data);
-      return (response.data as Map<String, dynamic>?, '');
+      final endpoint = Uri.parse('${client.baseUrl}/messages');
+      final response = await client.dio.getUri(endpoint);
+      return (response.data as List<dynamic>?, '');
     } catch (e) {
       return (null, e.toString());
     }
   }
 
-  static Future<(Map<String, dynamic>?, String)> postMessageToHospital(String hospitalId, Map<String, dynamic> data) async {
+  /// Get messages that correspond to a specific patient according to backend scope rules.
+  /// [patientId] Path parameter.
+  /// [limit] Optional query param to limit number of messages (default 10).
+  static Future<(List<dynamic>?, String)> getMessagesForPatient(String patientId, {int limit = 10}) async {
     try {
       final client = CococareApiClient.instance;
-      final endpoint = Uri.parse('${client.baseUrl}/messages/hospital/$hospitalId');
-      final response = await client.dio.postUri(endpoint, data: data);
-      return (response.data as Map<String, dynamic>?, '');
-    } catch (e) {
-      return (null, e.toString());
-    }
-  }
-
-  static Future<(Map<String, dynamic>?, String)> postMessageToStudy(String studyId, Map<String, dynamic> data) async {
-    try {
-      final client = CococareApiClient.instance;
-      final endpoint = Uri.parse('${client.baseUrl}/messages/study/$studyId');
-      final response = await client.dio.postUri(endpoint, data: data);
-      return (response.data as Map<String, dynamic>?, '');
+      final endpoint = Uri.parse('${client.baseUrl}/messages/patient/$patientId?limit=$limit');
+      final response = await client.dio.getUri(endpoint);
+      return (response.data as List<dynamic>?, '');
     } catch (e) {
       return (null, e.toString());
     }
